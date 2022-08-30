7b4714c9b39fa4b7465c1902bb59db5cc8d27b2c
Sugut <sugutvictorine@gmail.com>
2022-08-30 14:37:55 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
 function isPalindrome(word) {
   // Write your algorithm here
-}
+    const arrayValues = word.split('');
+    const reverseArrayValues = arrayValues.reverse();
+    const reverseString = reverseArrayValues.join('');
+
+    if(word === reverseString) {
+        console.log('True');
+    }
+    else {
+        console.log('False');
+    }
+  }
 
 /* 
   Add your pseudocode here
-*/
+ // convert string to an array
+ // reverse the array values
+ // convert array to string
 
 /*
-  Add written explanation of your solution here
+Add written explanation of your solution here
+// first we split the first letter
+// reeverse to check the last letter
+// use if condition to check if first and last string are same
+// return result
 */
 
 // You can run `node index.js` to view these console logs
